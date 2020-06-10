6eb19475f88911c8a406033cdc33ab2f6978e2aa
Brad Robinson <contact@toptensoftware.com>
2020-06-10 19:42:09 +1000

    Initial

---
+++ PlatformWin/implProgram.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace crossplat_test
+{
+    partial class Program
+    {
+        static string impl_GetPlatform() => "Windows";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace crossplat_test
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello World from {impl_GetPlatform()}!");
+        }
+    }
+}
